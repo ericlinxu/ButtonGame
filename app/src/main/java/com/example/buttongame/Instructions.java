688011207b28021f8daa4872e32ac856e1175aca
ericlx2 <ericlx2@illinois.edu>
2019-12-02 20:40:22 -0600

    so cool

--- app/src/main/java/com/example/buttongame/Instructions.java
+++ app/src/main/java/com/example/buttongame/Instructions.java
@@ -1,16 +1,29 @@
 package com.example.buttongame;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Instructions extends AppCompatActivity {
-    public void onCreate() {
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.how_to_play);
+        displayText();
+
+        Button back = findViewById(R.id.main);
+        back.setOnClickListener(unused -> startActivity(new Intent(this, MainActivity.class)));
+    }
+    public void displayText() {
         TextView how = findViewById(R.id.paragraph);
         TextView title = findViewById(R.id.title);
-        title.setText("How to Play");
-        how.setText("There are 21 counts in this game. You get to choose 1 or 2 sticks every turn. " +
+        String header = "How to Play";
+        title.setText(header);
+        String text = "There are 21 counts in this game. You get to choose 1 or 2 sticks every turn. " +
                 "In order to win, you cannot be the person to grab the last count. " +
-                "If you grab the last count, you lose.");
+                "If you grab the last count, you lose.";
+        how.setText(text);
     }
 }
--- app/src/main/java/com/example/buttongame/PlayGame.java
+++ app/src/main/java/com/example/buttongame/PlayGame.java
@@ -1,5 +1,6 @@
 package com.example.buttongame;
 
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,9 +9,15 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlayGame extends AppCompatActivity {
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_game);
+        play();
+    }
     public void play() {
         EditText numberSticks = findViewById(R.id.stickNum);
-
+        int number = 21;
+        numberSticks.setText(number.toString());
         Button restart = findViewById(R.id.restart);
         TextView lose = findViewById(R.id.lose);
         TextView win = findViewById(R.id.win);
