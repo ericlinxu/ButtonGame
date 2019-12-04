ad553dedaa1e84937d948cfea05e0d2e9c753cb5
ericlx2 <ericlx2@illinois.edu>
2019-12-03 21:30:12 -0600

    in construction

--- app/src/main/java/com/example/buttongame/MainActivity.java
+++ app/src/main/java/com/example/buttongame/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,16 +16,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final Button playGame = findViewById(R.id.playGame);
         final Button instructions = findViewById(R.id.instructions);
+        final Button comp = findViewById(R.id.computer);
+        final TextView name = findViewById(R.id.title);
         playGame.setVisibility(View.VISIBLE);
         instructions.setVisibility(View.VISIBLE);
+        comp.setOnClickListener(unused -> {
+            playGame.setVisibility(View.GONE);
+            instructions.setVisibility(View.GONE);
+            comp.setVisibility(View.GONE);
+            name.setVisibility(View.GONE);
+            startActivity(new Intent(this, PlayComp.class));
+        });
         playGame.setOnClickListener(unused -> {
             playGame.setVisibility(View.GONE);
             instructions.setVisibility(View.GONE);
+            comp.setVisibility(View.GONE);
+            name.setVisibility(View.GONE);
             startActivity(new Intent(this, PlayGame.class));
         });
         instructions.setOnClickListener(unused -> {
             playGame.setVisibility(View.GONE);
             instructions.setVisibility(View.GONE);
+            comp.setVisibility(View.GONE);
+            name.setVisibility(View.GONE);
             startActivity(new Intent(this, Instructions.class));
         });
     }
--- app/src/main/java/com/example/buttongame/PlayComp.java
+++ app/src/main/java/com/example/buttongame/PlayComp.java
@@ -2,16 +2,23 @@ package com.example.buttongame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PlayGame extends AppCompatActivity {
+public class PlayComp extends AppCompatActivity {
     private String number = "21";
     //public EditText numberSticks = findViewById(R.id.stickNum);
     //private int num = Integer.parseInt(numberSticks.getText().toString());
+
+    //private Button one = findViewById(R.id.one);
+    //private Button two = findViewById(R.id.two);
+    //private TextView lose = findViewById(R.id.lose);
+    //private TextView win = findViewById(R.id.win);
 
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,39 +52,70 @@ public class PlayGame extends AppCompatActivity {
             num--;
             number = String.valueOf(num);
             numberSticks.setText(String.valueOf(num));
-            play();
-            //computer();
+            //play();
+            //SystemClock.sleep(2000);
+            computer();
         });
         two.setOnClickListener(unused -> {
             int num = Integer.parseInt(number);
             num = num - 2;
             number = String.valueOf(num);
             numberSticks.setText(String.valueOf(num));
-            play();
-            //computer();
+            //play();
+            //SystemClock.sleep(2000);
+            computer();
         });
     }
-    /*public void computer() {
-        EditText numberSticks = findViewById(R.id.stickNum);
+    public void computer() {
+        TextView numberSticks = findViewById(R.id.stickNum);
+        TextView message = findViewById(R.id.compStick);
+        TextView win = findViewById(R.id.win);
+        TextView lose = findViewById(R.id.lose);
+        Button one = findViewById(R.id.one);
+        Button two = findViewById(R.id.two);
         int rand = (int) (Math.random() * 2) + 1;
-        if (number.equals("3")) {
+        if (number.equals("0")) {
+            one.setVisibility(View.GONE);
+            two.setVisibility(View.GONE);
+            lose.setVisibility(View.VISIBLE);
+        } else if(number.equals("1")) {
+            two.setVisibility(View.GONE);
+            int num = Integer.parseInt(number);
+            num--;
+            number = String.valueOf(num);
+            numberSticks.setText(String.valueOf(num));
+            String mess = "The computer took 1.";
+            message.setText(mess);
+            message.setVisibility(View.VISIBLE);
+            one.setVisibility(View.GONE);
+            win.setVisibility(View.VISIBLE);
+        } else if (number.equals("3")) {
             int num = Integer.parseInt(number);
             num = num - 2;
             number = String.valueOf(num);
             numberSticks.setText(String.valueOf(num));
+            String mess = "The computer took 2.";
+            message.setText(mess);
+            message.setVisibility(View.VISIBLE);
             play();
         } else if (number.equals("2")) {
             int num = Integer.parseInt(number);
             num--;
             number = String.valueOf(num);
             numberSticks.setText(String.valueOf(num));
+            String mess = "The computer took 1.";
+            message.setText(mess);
+            message.setVisibility(View.VISIBLE);
             play();
         } else {
             int num = Integer.parseInt(number);
             num -= rand;
             number = String.valueOf(num);
             numberSticks.setText(String.valueOf(num));
+            String mess = "The computer took " + rand;
+            message.setText(mess);
+            message.setVisibility(View.VISIBLE);
             play();
         }
-    }*/
+    }
 }
